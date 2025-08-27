ccecf04ba4a27aeb198a1e6f5ab266ba316700d6
Harsh Gupta <harsh.ixora@gmail.com>
2025-08-27 17:18:28 +0530

    auth added

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -13,17 +13,8 @@ const socialLogin = async (req, res) => {
             return res.status(400).json({ status: false, message: "uid and provider required" })
         }
 
-        // Upsert user
-        let user = await prisma.users.upsert({
-            where: { email },
-            update: {
-                name,
-                email,
-                profile: photoURL,
-                fcm_token,
-                login_type: provider.toUpperCase(),
-            },
-            create: {
+        let user = await prisma.users.create({
+            data: {
                 uid,
                 name,
                 email,
@@ -31,10 +22,41 @@ const socialLogin = async (req, res) => {
                 fcm_token,
                 login_type: provider.toUpperCase(),
                 password: "",
-            },
-        })
+                role: "LEARNER"
+            }
+        });
 
-        // Generate token
+        return res.json({
+            status: true,
+            message: "User Created successfully",
+        })
+    } catch (err) {
+        return res.status(500).json({ status: false, message: err.message })
+    }
+}
+
+
+const fetchUserData = async (req, res) => {
+    try {
+        const { uid, email } = req.body
+
+        if (!uid || !email) {
+            return res.status(400).json({ status: false, message: "uid and email required" })
+        }
+
+        const user = await prisma.users.findFirst({
+            where: { uid, email },
+        });
+
+        if (!user) {
+            return res.json({
+                status: false,
+                code: "USER_NOT_FOUND",
+                message: "User not found"
+            })
+        }
+
+
         const jwtToken = generateToken(user.id)
 
         await prisma.users.update({
@@ -44,7 +66,7 @@ const socialLogin = async (req, res) => {
 
         return res.json({
             status: true,
-            message: "Login successful",
+            message: "User Fetched successfully",
             data: { jwtToken, user },
         })
     } catch (err) {
@@ -52,9 +74,9 @@ const socialLogin = async (req, res) => {
     }
 }
 
-const fetchUserData = async (req, res) => {
+const updateFcmToken = async (req, res) => {
     try {
-        const { uid, email } = req.body
+        const { fcm_token, email, uid } = req.body
 
         if (!uid || !email) {
             return res.status(400).json({ status: false, message: "uid and email required" })
@@ -65,18 +87,14 @@ const fetchUserData = async (req, res) => {
             where: { uid, email }
         })
 
-        // Generate token
-        const jwtToken = generateToken(user.id)
-
         await prisma.users.update({
             where: { id: user.id },
-            data: { token: jwtToken },
+            data: { fcm_token: fcm_token },
         })
 
         return res.json({
             status: true,
-            message: "User Fetched successful",
-            data: { jwtToken, user },
+            message: "Token Updated successful",
         })
     } catch (err) {
         return res.status(500).json({ status: false, message: err.message })
@@ -85,26 +103,58 @@ const fetchUserData = async (req, res) => {
 
 
 async function getOnboardingQuestions(req, res) {
-  try {
-    const questions = await prisma.onboarding_questions.findMany({
-      include: {
-        onboarding_options: true, // 👈 pulls all related options
-      },
-      orderBy: { id: 'asc' }, // optional, to keep consistent ordering
-    });
+    try {
+        const questions = await prisma.onboarding_questions.findMany({
+            include: {
+                onboarding_options: true, // 👈 pulls all related options
+            },
+            orderBy: { id: 'asc' }, // optional, to keep consistent ordering
+        });
 
-    return res.json({
-      status: true,
-      message: "Onboarding questions fetched successfully",
-      data: questions,
-    });
-  } catch (err) {
-    console.error("Error fetching onboarding questions:", err);
-    return res.status(500).json({
-      status: false,
-      message: err.message,
-    });
-  }
+        return res.json({
+            status: true,
+            message: "Onboarding questions fetched successfully",
+            data: questions,
+        });
+    } catch (err) {
+        console.error("Error fetching onboarding questions:", err);
+        return res.status(500).json({
+            status: false,
+            message: err.message,
+        });
+    }
 }
 
-module.exports = { socialLogin, fetchUserData, getOnboardingQuestions }
+const submitOnboarding = async (req, res) => {
+    try {
+        const { userId, metadata, questionnaire } = req.body;
+
+        const answersArray = Object.entries(questionnaire).map(([key, value]) => ({
+            question_key: key,
+            answer_value: value
+        }));
+
+        const response = await prisma.onboarding_responses.create({
+            data: {
+                user_id: Number(userId),
+                build_no: metadata.buildNo,
+                build_signature: metadata.buildSignature,
+                version: metadata.version,
+                completed_at: new Date(metadata.completedAt),
+                onboarding_answers: {
+                    create: answersArray
+                }
+            },
+            include: {
+                onboarding_answers: true
+            }
+        });
+
+        return res.json({ status: true, message: "Onboarding submitted", data: response });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ status: false, message: err.message });
+    }
+};
+
+module.exports = { socialLogin, fetchUserData, updateFcmToken, getOnboardingQuestions, submitOnboarding }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { socialLogin, fetchUserData, getOnboardingQuestions } = require('../controller/auth.controller');
+const { socialLogin, fetchUserData,updateFcmToken, getOnboardingQuestions, submitOnboarding } = require('../controller/auth.controller');
 const router = express.Router();
 
 router.post('/auth/social-login', socialLogin);
-router.post('/auth/getProfile', fetchUserData);
-router.post('/auth/getOnboardingQuestions', getOnboardingQuestions);
+router.post('/auth/fetchUserData', fetchUserData);
+router.post('/auth/updateFcmToken', updateFcmToken);
+router.get('/auth/getOnboardingQuestions', getOnboardingQuestions);
+router.post("/auth/submitOnboarding", submitOnboarding);
 
 module.exports = router;
