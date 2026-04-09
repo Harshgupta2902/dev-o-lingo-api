3f25b632086ae1a60651605f167fedf620368110
Harsh Gupta <harsh.ixora@gmail.com>
2026-04-09 21:12:23 +0530

    refactor: rename shop order creation route to /shop/create

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -4,7 +4,7 @@ const auth = require("../middleware");
 
 // Shop APIs
 router.get("/shop/items", getShopItems);
-router.post("/shop/orders", auth, createOrder);
+router.post("/shop/create", auth, createOrder);
 router.post("/shop/verify", auth, verifyPurchase);
 
 module.exports = router;
